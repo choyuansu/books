acc96af971b856dccbc82326091d25e2081dff2e
Alex Su <choyuansu@gmail.com>
2024-11-25 10:05:02 -0800

    UI improvements and unfinished text to speech feature

--- src/components/Reader.vue
+++ src/components/Reader.vue
@@ -1,15 +1,23 @@
 <script setup>
-import { ref, onMounted, watch } from 'vue'
+import { ref, onMounted, watch, computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 
 import { db } from '@/db'
 
-import { Button } from 'primevue'
+import Button from 'primevue/button'
+import Drawer from 'primevue/drawer'
 
 const route = useRoute()
 const router = useRouter()
 
-const chapter = ref({ content: '' })
+const synth = window.speechSynthesis
+
+const chapter = ref({ title: '', content: '' })
+const drawerVisible = ref(false)
+
+const paragraphs = computed(() => {
+  return chapter.value.content.split('\n').map((p) => p.trim())
+})
 
 const previousPage = () => {
   router.push(`/book/${route.params.bookId}/ch/${parseInt(route.params.chapterId) - 1}`)
@@ -28,14 +36,39 @@ const getChapterData = async (bookId, chapterId) => {
   })
 }
 
+function startSpeechSynthesis(index) {
+  console.log(synth.paused, synth.speaking)
+  if (synth.paused) {
+    synth.resume()
+    return
+  } else if (synth.speaking) {
+    synth.cancel()
+    return
+  }
+  const voice = synth.getVoices().filter((voice) => voice.lang == 'zh-TW')[0]
+  try {
+    for (const paragraph of paragraphs.value.slice(index)) {
+      let utterance = new SpeechSynthesisUtterance(paragraph)
+      utterance.lang = 'zh-TW'
+      utterance.voice = voice
+      synth.speak(utterance)
+    }
+  } catch (err) {
+    console.error(err)
+  }
+}
+
 watch(
   [() => route.params.bookId, () => route.params.chapterId],
   async ([newBookId, newChapterId]) => {
+    synth.cancel()
     getChapterData(parseInt(newBookId), parseInt(newChapterId))
     db.books.update(parseInt(newBookId), { currentChapter: parseInt(newChapterId) })
+    window.scrollTo(0, 0)
   },
 )
 onMounted(async () => {
+  synth.cancel()
   getChapterData(parseInt(route.params.bookId), parseInt(route.params.chapterId))
   db.books.update(parseInt(route.params.bookId), {
     currentChapter: parseInt(route.params.chapterId),
@@ -44,8 +77,9 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="flex justify-center m-4">
-    <span class="text-4xl font-bold">{{ chapter.title }}</span>
+  <div class="flex flex-col justify-center m-4 h-36">
+    <span class="text-4xl font-bold">{{ chapter.title.split(' ')[0] }}</span>
+    <span class="text-4xl font-bold">{{ chapter.title.split(' ')[1] }}</span>
   </div>
   <div class="flex justify-between m-2">
     <Button label="上一頁" severity="secondary" @click="previousPage" />
@@ -53,11 +87,32 @@ onMounted(async () => {
     <Button label="下一頁" severity="secondary" @click="nextPage" />
   </div>
   <div class="m-4 text-2xl">
-    <p v-for="paragraph in chapter.content.split('\n')" class="indent-8 my-6">{{ paragraph }}</p>
+    <p
+      v-for="(paragraph, index) in paragraphs"
+      class="indent-8 my-6 select-none"
+      @click="startSpeechSynthesis(index)"
+    >
+      <mark class="bg-transparent text-white hover:bg-sky-800 hover:cursor-pointer">
+        {{ paragraph }}
+      </mark>
+    </p>
   </div>
   <div class="flex justify-between m-2">
     <Button label="上一頁" severity="secondary" @click="previousPage" />
     <Button label="下一頁" severity="secondary" @click="nextPage" />
+  </div>
+  <div class="w-full h-20"></div>
+
+  <Drawer v-model:visible="drawerVisible" position="bottom" header="Speech" :modal="false">
+    <div class="flex gap-2">
+      <Button icon="pi pi-play" severity="secondary" rounded @click="startSpeechSynthesis(0)" />
+      <Button icon="pi pi-pause" severity="secondary" rounded @click="synth.pause()" />
+      <Button icon="pi pi-stop" severity="secondary" rounded @click="synth.cancel()" />
+    </div>
+  </Drawer>
+
+  <div class="fixed bottom-4 right-4">
+    <Button icon="pi pi-cog" severity="secondary" rounded @click="drawerVisible = true" />
   </div>
 </template>
 
